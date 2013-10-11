5df79a21541de5f6bbc94a5dc11e1c6704b8d82c
Felicia Liiv <felicia.liiv@elev.it-gymnasiet.se>
2013-10-11 10:04:25 +0200

    Alla uppgifter i kapitell elva förutom sista.

---
+++ Programmering 1/ConsoleApplication19/ConsoleApplication19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        public static void Hello(string message)
+        {
+            Console.WriteLine("Hej du skrev: " + message);
+        }
+
+        static void Main(string[] args)
+        {
+            string inm = Console.ReadLine();
+            Hello(inm);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programmering 1/ConsoleApplication23/ConsoleApplication23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication23
+{
+    class Program
+    {
+        public static double moms(double pris, double momsProcent)
+        {
+
+            momsProcent = (momsProcent / 100) + 1;
+            double medMoms = pris * momsProcent;
+
+            return medMoms;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(moms(100, 34));
+
+            Console.ReadKey();
+        }
+    }
+}
